9ee6f3a1026924a3d6d1347dd44ed2d0b3f3c15d
Andrew Smyelov <andrew.smyelov@gmail.com>
2022-08-27 20:47:05 -0700

    init

---
+++ Helpers/helper.page.js
@@ -0,0 +1,32 @@
+exports.DelekHomePage = class DelekHomePage {
+    constructor(page) {
+      this.page = page;
+
+    //! EXAMPLES
+    //   this.clickDiscoverBtn = page.locator("text=Discover");
+    //   this.mostPopularJobsDisplayed = page.locator('[class="col-12 text-center"]');
+    //   this.clickFaqMenu = page.locator('[href="/faq"]');
+    //   this.whatIsGigturboTxtVisible = page.locator("text=What is gigturbo?");
+    //   this.whyNowTxtVisible = page.locator("text=Why now?");
+    //   this.whatIsForTxtVisible = page.locator("text=Who gigturbo is for");
+    //   this.usingYourPATxtVisible = page.locator("text=Using your Personal AI Assistant");
+    //   this.letsGetStartedBtn = page.locator(".col-11");
+    //   this.getYouStartedTxtVisible = page.locator(".signup_contentContainer__35-G7 > .row > .col > h1");
+    //   this.logInBtn = page.locator("text=Log In");
+    //   this.welcomeBackTxtVisible = page.locator('h1:has-text("Welcome Back!")');
+    //   this.logInPhoneNumber = page.locator('[class="col-12 text-center"]');
+    //   this.phoneNumTxtBookVisible = page.locator("#telephone");
+    //   this.typeInTheSearchBox = page.locator("[placeholder='Search']");
+    //   this.onlyPetCareIsDisplayed = page.locator('[class="List_listContainer__1QluD "]');
+    //   this.selectPetCareIcon = page.locator("text=Pet Care");
+    //   this.nextBtn = page.locator(".col-2");
+    //   this.sixJobIconsDisplayed = page.locator('[class="col-4 col-sm-auto d-flex justify-content-center"]');
+    //   this.madPawsIcon = page.locator('[alt="MadPaws"]');
+    //   this.checkMadPawsTitle = page.locator("text=MadPaws");
+    //   this.checkWorkLocationAustralia = page.locator("text=Work Locations: Australia");
+    //   this.checkJobCategoriesPetCare = page.locator("text=Job Categories: Pet Care");
+    //   this.checkHourlyRate$18 = page.locator("text=Hourly Rate: $18");
+
+
+    }
+};
---
+++ tests/tests.spec.js
@@ -0,0 +1,39 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const { DelekHomePage } = require('../Helpers/helper.page');
+let delekHomePage
+
+test.beforeEach(async ({ page }) => {
+  delekHomePage = new DelekHomePage(page);
+  await page.goto('https://delekhomes.com/');
+});
+
+test('test name', async ({ page }) => {
+
+
+//! EXAMPLES
+// await delekHomePage.clickDiscoverBtn.click();
+// await expect(page).toHaveURL("https://dev.gigturbo.com/discover");
+// await expect(delekHomePage.mostPopularJobsDisplayed).toHaveText('The most popular platforms and jobs');
+// await delekHomePage.clickFaqMenu.click();
+// await expect(page).toHaveURL("https://dev.gigturbo.com/faq");
+// await expect(delekHomePage.whatIsGigturboTxtVisible).toHaveCount(1);
+// await expect(delekHomePage.whyNowTxtVisible).toHaveCount(1);
+// await expect(delekHomePage.whatIsForTxtVisible).toHaveCount(1);
+// await expect(delekHomePage.usingYourPATxtVisible).toHaveCount(1);
+// await delekHomePage.letsGetStartedBtn.click();
+// await expect(page).toHaveURL("https://dev.gigturbo.com/signup");
+// await expect(delekHomePage.getYouStartedTxtVisible).toHaveText("Let's get you started!");
+// await delekHomePage.logInBtn.click();
+// await expect(page).toHaveURL("https://dev.gigturbo.com/login");
+// await expect(delekHomePage.welcomeBackTxtVisible).toHaveText('Welcome Back!');
+// await expect(delekHomePage.logInPhoneNumber).toHaveText('Log in with your phone number');
+// await expect(delekHomePage.phoneNumTxtBookVisible).toHaveCount(1);
+// await delekHomePage.typeInTheSearchBox.fill("Pet Care");
+// await expect(delekHomePage.onlyPetCareIsDisplayed).toHaveCount(1);
+// await delekHomePage.selectPetCareIcon.click();
+// await delekHomePage.nextBtn.click();
+// await delekHomePage.madPawsIcon.click();
+
+
+});
